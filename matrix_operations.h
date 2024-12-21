a00d5efd7fcff3a9e9db2cf1abb59e690979ceff
Ved Vyas <vedvyascodes1608@gmail.com>
2024-12-21 17:57:46 +0530

    matrix_operations.h

--- matrix_operations.h
+++ matrix_operations.h
@@ -7,7 +7,7 @@
 int trace_matrix(int **matrix, int size);
 void transpose_matrix(int **matrix, int **transpose, int rows,int cols);
 int matrix_determinant(int **matrix, int size);
-int matrix_adjoint(int **matrix,int **result, int size);
+void adjoint_matrix(int **matrix,int **result, int size);
 int matrix_inverse(int **matrix, int **inverse, int size);
 
 #endif
